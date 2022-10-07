45c23c4d41dd1cd1990caf58bb1081303003e389
Mikhail Kulikov <mikhail.kulikov@databricks.com>
2022-10-07 17:38:15 +0200

    fix

--- update_fare_table.sql
+++ update_fare_table.sql
@@ -1,4 +1,4 @@
-INSERT INTO TABLE daily_fare_table
+CREATE TABLE daily_fare_table as
 SELECT
   T.weekday,
   CASE
@@ -22,11 +22,11 @@ FROM
       `samples`.`nyctaxi`.`trips`
     WHERE
       (
-        pickup_zip in (10001, 10002)
+        pickup_zip in (10001)
         OR pickup_zip in (10018)
       )
-      AND tpep_pickup_datetime BETWEEN TIMESTAMP '2016-01-16 12:07'
-      AND TIMESTAMP '2017-01-16 12:07'
+      AND tpep_pickup_datetime BETWEEN TIMESTAMP '2016-01-01 12:07'
+      AND TIMESTAMP '2016-01-16 12:07'
       AND trip_distance < 10
   ) T
 ORDER BY
