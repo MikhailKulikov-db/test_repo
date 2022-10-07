5675311c198e1e435f3950ec0eaa06d4095c40f3
Mikhail Kulikov <mikhail.kulikov@databricks.com>
2022-10-07 20:36:07 +0200

    fix

--- update_fare_table.sql
+++ update_fare_table.sql
@@ -1,12 +1,16 @@
-INSERT INTO TABLE daily_fare_table
-SELECT
-  dayofweek(tpep_pickup_datetime) as weekday,
-  *
-FROM
-  nyctaxi.trips
-WHERE
-  pickup_zip in (10001, 10018)
-  AND tpep_pickup_datetime BETWEEN TIMESTAMP '2016-01-16 12:07'
-  AND TIMESTAMP '2017-01-16 12:07'
-  AND trip_distance < 10
+MERGE INTO TABLE daily_fare_table
+USING (
+  SELECT
+    day(tpep_pickup_datetime) as day,
+    count(*) as daily_trip_count
+  FROM
+    samples.nyctaxi.trips
+  WHERE
+    tpep_pickup_datetime BETWEEN TIMESTAMP '2016-01-01 12:07'
+    AND TIMESTAMP '2016-01-14 12:07'
+  group by day(tpep_pickup_datetime)
+) as source
+on daily_fare_table.day == source.day and daily_fare_table.day in (12, 13, 14)
+when matched then
+  update set daily_fare_table.daily_trip_count = source.daily_trip_count
 ;
