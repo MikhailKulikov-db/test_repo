dd5bb417a72fd2353c3766214dd708930e85881a
Mikhail Kulikov <mikhail.kulikov@databricks.com>
2022-10-07 18:35:40 +0200

    fix

--- create_fare_table.sql
+++ create_fare_table.sql
@@ -6,10 +6,7 @@ SELECT
 FROM
     `samples`.`nyctaxi`.`trips`
 WHERE
-    (
-    pickup_zip in (10001)
-    OR pickup_zip in (10018)
-    )
+    pickup_zip in (10001, 10018)
     AND tpep_pickup_datetime BETWEEN TIMESTAMP '2016-01-01 12:07'
     AND TIMESTAMP '2016-01-16 12:07'
     AND trip_distance < 10
